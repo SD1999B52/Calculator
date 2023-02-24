97cad7ee1d9f1f99e1799373deef47e62f7fcb8f
Egor <yofilo7713@aosod.com>
2023-02-24 12:27:42 +0500

    Создан файл заголовка для класса Calculator

---
+++ Calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+float plus(float fTerm, float sTerm);
+float minus(float minuend, float subtrahend);
+float divide(float dividend, float divider);
+float multiply(float multiplicand, float multiplier);
+
+#endif
